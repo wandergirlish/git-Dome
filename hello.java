c71425b6fd804cb2028088336fef95bb649430ce
wandergirlish <saberqqcom@qq.com>
2024-05-13 20:03:55 +0800

    Update hello.java
    second commit

--- hello.java
+++ hello.java
@@ -2,6 +2,18 @@ public class hello{
 	public static void main(String [] args)
 	{
 		System.out.println("hello world");
+		student s1 =new student();
+		s1.show();
+		s1.test();
 	}
-
+}
+class student{
+	public void show()
+	{
+		System.out.println("my name is Yuuka");
+	}
+	public void test()
+	{
+		Sustem.out.println("Yuuka math score is 99");
+	}
 }
